921d5436609f8c3c933830142968f7cbce5a307c
Michal Stora <stora.michal@gmail.com>
2015-10-29 00:47:07 +0100

    Now computing Dr for each window

--- Tubular.java
+++ Tubular.java
@@ -11,6 +11,7 @@ public class Tubular implements PlugInFilter {
 	private int numOfProbes = 100;
 	private String title;
 	private int numOfWindows = 3; /*the number will be squared*/
+	private int dNeighboursDistance = 4; /*length od the d-neighobour distance*/
 
 	public int setup(String arg, ImagePlus im) 
 	{
@@ -61,7 +62,7 @@ public class Tubular implements PlugInFilter {
 		int start = 0;
 		int idx = 0;
 		int total = 0;
-		int percentage = count / 5;
+		int percentage = histMax / 5;
 		for (int i = 0; i < hist.length; ++i)
 		{
 			start = histImgHeight - (int)((double) hist[i] / histMax * histImgHeight);
@@ -94,31 +95,75 @@ public class Tubular implements PlugInFilter {
 	}	
 
 	private int[] PlantProbes(ImageProcessor ip){
-		int[] result = new int[256]; 
+		int[] result = new int[8]; 
+		int[] tmp;
 		int width = ip.getWidth();
 		int height = ip.getHeight();
 		Random rnd = new Random();
 		int posx,posy;
 		for (int i = 0 ; i< numOfProbes;i++){
-			posx = rnd.nextInt(width+1);
-			posy = rnd.nextInt(height+1);
-			result[ip.get(pos)]++;  
+			do {
+				posx = rnd.nextInt(width);
+			    posy = rnd.nextInt(height);} 
+			while( ip.get(posx,posy) == 0);
+			tmp = dNeighbours(ip,posx,posy,dNeighboursDistance);
+			for (int j = 0; j<8;j++){
+				result[j]+=tmp[j];
+			}
 		}
 
-		/*not supported yet*/
 		return result;
+	}
+	/* 0 is left middle, 1 is left upper, 2 is upper, 3 is right upper, 4 is right middle, 5 is right down, 6 is down, 7 is left down*/
+	private int[] dNeighbours(ImageProcessor ip, int x, int y, int distance){
+		int[] result = new int[8];
+		result[0] = getNeighbour(ip , x-distance, y);
+		result[1] = getNeighbour(ip, x-distance,y-distance);
+		result[2] = getNeighbour(ip,x,y-distance);
+		result[3] = getNeighbour(ip,x+distance,y-distance);
+		result[4] = getNeighbour(ip,x+distance,y);
+		result[5] = getNeighbour(ip,x+distance,y+distance);
+		result[6] = getNeighbour(ip,x,y+distance);
+		result[7] = getNeighbour(ip,x-distance,y+distance);
+		return result;
+	}	
+
+	int getNeighbour(ImageProcessor ip, int x, int y){
+		int width = ip.getWidth();
+		int height = ip.getHeight();
+		if( (x<width)&&(x>0)&&(y<height)&&(y>0)){
+			return ip.get(x,y) > 0 ? 1 : 0;
+		}else{return 0;}
+	}
+
+	int[] getMinMax(int[] input){
+		int min = numOfProbes+1;
+		int max = 0;
+
+		for (int i = 0; i < input.length;i++){
+			if (input[i]<min){min = input[i];}
+			if (input[i]>max){max = input[i];}
+		}
+		int[] res={min,max};
+		return res;
 	}
 
 	private void cropImage(ImageProcessor ip){
 		int cropWidth = ip.getWidth() / numOfWindows;
 		int cropHeight = ip.getHeight() / numOfWindows;
+		int[] res;
+		int[] minMax;
+		float Dr;
 		ImageProcessor cropped;
 		for (int i = 0; i < numOfWindows; i++){
 			for (int j = 0; j<numOfWindows;j++){
 				ip.setRoi(cropWidth * i, cropHeight * j, cropWidth, cropHeight);
 				cropped = ip.crop();
-				int[] = PlantProbes(cropped);
-				new ImagePlus("croppedImage" + i +" " + j, cropped).show();
+				res = new int[8];
+				res= PlantProbes(cropped);
+				minMax = getMinMax(res);
+				Dr = minMax[1] / minMax[0];
+				new ImagePlus("croppedImage" + i +" " + j + " Dr :" + Dr, cropped).show();
 			}
 		}
 
